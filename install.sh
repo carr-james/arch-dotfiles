25b6ed1799389664da3cb6262cb6a9497e319365
James Carr <jamescarr1993@gmail.com>
2021-08-01 22:37:26 +0100

    Customize fonts and add dunst notifications

--- install.sh
+++ install.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-DOTFILES=$HOME/.dotfiles
+source ./zsh/.zshenv
 
 # nvim
-mkdir -p "$HOME/.config/nvim"
-mkdir -p "$HOME/.config/nvim/undo"
-ln -sf "$DOTFILES/nvim/init.vim" "$HOME/.config/nvim"
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
+ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 
 # X11
-rm -rf "$HOME/.config/X11"
-ln -s "$DOTFILES/X11" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/X11"
+ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
 # i3
-rm -rf "$HOME/.config/i3"
-ln -s "$DOTFILES/i3" "$HOME/.config"
+rm -rf "$XDG_CONFIG_HOME/i3"
+ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
 # zsh
-mkdir -p "$HOME/.config/zsh"
+mkdir -p "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
-ln -sf "$DOTFILES/zsh/.zprofile" "$HOME/.config/zsh"
+ln -sf "$DOTFILES/zsh/.zprofile" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/.zprofile" "$HOME"
-ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.config/zsh"
-ln -sf "$DOTFILES/zsh/aliases" "$HOME/.config/zsh/aliases"
-rm -rf "$HOME/.config/zsh/external"
-ln -sf "$DOTFILES/zsh/external" "$HOME/.config/zsh"
-ln -sf "$DOTFILES/zsh/scripts.zsh" "$HOME/.config/zsh"
+ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
+rm -rf "$XDG_CONFIG_HOME/zsh/external"
+ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/scripts.zsh" "$XDG_CONFIG_HOME/zsh"
+
+# fonts
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+
+# dunst
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
